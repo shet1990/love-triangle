12d5c35d5ddf641dcecdb7e2eb3cc82767ce6397
shet1990 <shet1990@gmail.com>
2018-09-27 23:12:26 +0300

    love-triangle/decision

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let count = 0;
+
+    for (let i = 0, len = preferences.length; i <= len; i++) {
+        const firstPos = preferences[i];
+        const secondPos = preferences[firstPos - 1];
+        const thirdPos = preferences[secondPos - 1];
+
+        const conditionOne = thirdPos === i + 1;
+        const conditionTwo = firstPos !== i + 1;
+        if (conditionOne && conditionTwo) {
+            count++;
+        }
+    }
+    return count / 3;
 };
